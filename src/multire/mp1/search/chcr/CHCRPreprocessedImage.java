55f16a7f2a0f1b1e7e84acb89275b06966eb1297
Earl <Earl@192.168.1.104>
2015-07-26 22:24:16 +0800

    recreated CHCR search strategy

--- src/multire/mp1/search/chcr/CHCRPreprocessedImage.java
+++ src/multire/mp1/search/chcr/CHCRPreprocessedImage.java
@@ -4,71 +4,47 @@
  */
 package multire.mp1.search.chcr;
 
-import java.awt.Color;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.imageio.ImageIO;
 import multire.mp1.search.PreprocessedImage;
-import multire.mp1.cie.cieConvert;
-import java.awt.image.ColorModel;
+import java.util.List;
 
 /**
  *
  * @author Earl
  */
-public class CHCRPreprocessedImage extends PreprocessedImage{
-    
-    private void ColorHistogramCenterRefMethod(int [] histogramQCen, int [] histogramQNonCen, int [] histogramTMCen, int [] histogramTMNonCen) {
-        int totalPQCen = 0, totalPQNonCen = 0, totalPMCen = 0, totalPMNonCen = 0;
-        
-        double[] NHACenter = new double[4];
-        double[] NHBCenter = new double[4];
-        double[] NHANonCenter = new double[4];
-        double[] NHBNonCenter = new double[4];
-        double[] NHcompareCenter = new double[4];
-        double[] NHcompareNonCenter = new double[4];
-        double similarityABcenter = 0, similarityABnoncenter = 0; 
-        
-        //for center area
-        for(int i=0; i < 4; i++){ totalPQCen += histogramQCen[i]; }
-        for(int j=0; j < 4; j++){ totalPMNonCen += histogramTMNonCen[j];}
-        
-        for(int i=0; i < 4; i++){
-            NHACenter[i] = histogramQCen[i] / totalPQCen;
-        }
-        for(int i=0; i < 4; i++){
-            NHBCenter[i] = histogramTMCen[i] / totalPMCen;
-        }
-        for(int i=0; i < 4; i++){
-            if(NHACenter[i] > NHBCenter[i]){ NHcompareCenter[i] = NHACenter[i]; }
-            else { NHcompareCenter[i] = NHBCenter[i]; }
-        }
-        for(int i=0; i < 4; i++){
-            similarityABcenter += 1 - (Math.abs(NHACenter[i] - NHBCenter[i]) / NHcompareCenter[i]);
-        }
-        
-        //for non-center area
-        for(int i=0; i < 4; i++){ totalPQNonCen += histogramQNonCen[i]; }
-        for(int j=0; j < 4; j++){ totalPMNonCen += histogramTMNonCen[j];}
-        
-        for(int i=0; i < 4; i++){
-            NHANonCenter[i] = histogramQNonCen[i] / totalPQNonCen;
-        }
-        for(int i=0; i < 4; i++){
-            NHBNonCenter[i] = histogramTMNonCen[i] / totalPMNonCen;
-        }
-        for(int i=0; i < 4; i++){
-            if(NHANonCenter[i] > NHBNonCenter[i]){ NHcompareNonCenter[i] = NHANonCenter[i]; }
-            else { NHcompareNonCenter[i] = NHBNonCenter[i]; }
-        }
-        for(int i=0; i < 4; i++){
-            similarityABnoncenter += 1 - (Math.abs(NHANonCenter[i] - NHBNonCenter[i]) / NHcompareNonCenter[i]);
-        }
-        
-        similarityABcenter /= 4;
-        similarityABnoncenter /= 4;
+public class CHCRPreprocessedImage extends PreprocessedImage {
+    private List<Integer> histogramBinsCenter;
+    private List<Integer> histogramBinsNonCenter;
+
+    /**
+     * @return the histogramBinsCenter
+     */
+    public List<Integer> getHistogramBinsCenter() {
+        return histogramBinsCenter;
     }
+
+    /**
+     * @param histogramBinsCenter the histogramBinsCenter to set
+     */
+    public void setHistogramBinsCenter(List<Integer> histogramBinsCenter) {
+        this.histogramBinsCenter = histogramBinsCenter;
+    }
+
+    /**
+     * @return the histogramBinsNonCenter
+     */
+    public List<Integer> getHistogramBinsNonCenter() {
+        return histogramBinsNonCenter;
+    }
+
+    /**
+     * @param histogramBinsNonCenter the histogramBinsNonCenter to set
+     */
+    public void setHistogramBinsNonCenter(List<Integer> histogramBinsNonCenter) {
+        this.histogramBinsNonCenter = histogramBinsNonCenter;
+    }
+
+    /**
+     * @return the histogramBins
+     */
+    
 }
---
+++ src/multire/mp1/search/chcr/CHCRSearchStrategy.java
@@ -0,0 +1,102 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package multire.mp1.search.chcr;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import javax.imageio.ImageIO;
+import multire.mp1.cie.cieConvert;
+import multire.mp1.search.PreprocessedImage;
+import multire.mp1.search.SearchStrategy;
+
+/**
+ *
+ * @author lugkhast
+ */
+public class CHCRSearchStrategy implements SearchStrategy {
+
+    public final int BIN_COUNT = 159;
+
+    @Override
+    public double compareImageFiles(PreprocessedImage queryImage, PreprocessedImage datasetImage) {
+        int totalDifferenceCenter = 0, totalDifferenceNonCenter = 0, totalDifference;
+        List<Integer> queryBinsCenter, dsBinsCenter, queryBinsNonCenter, dsBinsNonCenter;
+        CHCRPreprocessedImage chQueryImg = (CHCRPreprocessedImage) queryImage;
+        CHCRPreprocessedImage chDatasetImg = (CHCRPreprocessedImage) datasetImage;
+        
+        queryBinsCenter = chQueryImg.getHistogramBinsCenter();
+        dsBinsCenter = chDatasetImg.getHistogramBinsCenter();
+        queryBinsNonCenter = chQueryImg.getHistogramBinsNonCenter();
+        dsBinsNonCenter = chDatasetImg.getHistogramBinsNonCenter();
+        
+        for (int i = 0; i < BIN_COUNT; i++) {
+            totalDifferenceCenter += Math.abs(
+                    queryBinsCenter.get(i) - dsBinsCenter.get(i)
+            );
+            totalDifferenceNonCenter += Math.abs(
+                    queryBinsNonCenter.get(i) - dsBinsNonCenter.get(i)
+            );
+        }
+        
+        return totalDifferenceCenter;
+    }
+
+    private int getBinFromRGB(int rgb) {
+        int red, green, blue;
+        Color color = new Color(rgb);
+        cieConvert converter = new cieConvert();
+
+        red = color.getRed();
+        green = color.getGreen();
+        blue = color.getBlue();
+
+        converter.setValues(red / 255.0, green / 255.0, blue / 255.0);
+        return converter.IndexOf();
+    }
+
+    @Override
+    public PreprocessedImage preprocessImage(File imageFile) throws IOException {
+        CHCRPreprocessedImage preppedImage = new CHCRPreprocessedImage();
+        BufferedImage image = ImageIO.read(imageFile);
+        Integer[] colorBinsCenter = new Integer[BIN_COUNT];
+        Integer[] colorBinsNonCenter = new Integer[BIN_COUNT];
+        int bin, rgb;
+        int cenlimitHeight, cenlimitWidth;
+        
+        cenlimitHeight = (int) Math.ceil(image.getHeight()/.125);
+        cenlimitWidth = (int) Math.ceil(image.getWidth()/.125);
+        Arrays.fill(colorBinsCenter, 0);
+        Arrays.fill(colorBinsNonCenter, 0);
+        preppedImage.setImageFile(imageFile);
+        preppedImage.setImage(image);
+
+        for (int i = 0; i < image.getWidth(); i++) {
+            for (int j = 0; j < image.getHeight(); j++) {
+                rgb = image.getRGB(i, j);
+                bin = getBinFromRGB(rgb);
+                if((j >= cenlimitHeight || j <= image.getHeight() - cenlimitHeight) && 
+                  ((i >= cenlimitWidth || i <= image.getWidth() - cenlimitWidth))){
+                        colorBinsCenter[bin] += 1;
+                }
+                else { colorBinsNonCenter[bin] += 1; }
+            }
+        }
+        
+        preppedImage.setHistogramBinsCenter(Arrays.asList(colorBinsCenter));
+        preppedImage.setHistogramBinsNonCenter(Arrays.asList(colorBinsNonCenter));
+
+        return preppedImage;
+    }
+    
+    @Override
+    public String toString() {
+        return "Color Histogram with Centering Refinement";
+    }
+
+}
